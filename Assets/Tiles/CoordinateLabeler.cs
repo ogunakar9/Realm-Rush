3a1f57b3b0722b28606b1c0a00565576f18f13b0
ogunakar9 <ogunakar09@gmail.com>
2021-07-18 15:15:44 +0300

    Add PathFinder exploring neighbors

--- Assets/Tiles/CoordinateLabeler.cs
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -7,16 +7,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.grey;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(0.84f, 0.42f, 0f);
     
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    private GridManager gridManager;
+    
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
         
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -42,7 +45,34 @@ public class CoordinateLabeler : MonoBehaviour
     
     void SetLabelColor()
     {
-        label.color = waypoint.IsPlaceable ? defaultColor : blockedColor;
+        if (gridManager == null)
+        {
+            return;
+        }
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null)
+        {
+            return;
+        }
+        
+        if (!node.isWalkable)
+        {
+            label.color = blockedColor;
+        }
+        else if (node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if (node.isExplored)
+        {
+            label.color = exploredColor;
+        }
+        else
+        {
+            label.color = defaultColor;
+        }
     }
 
     void DisplayCoordinates()
